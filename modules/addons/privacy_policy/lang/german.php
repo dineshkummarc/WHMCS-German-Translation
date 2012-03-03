8cb3c9b9a3bca04e27a42c8b16045d3e473b3c46
Torsten Widmann <info@goover.de>
2012-03-03 07:44:57 +0100

    privacy_policy/lang/german.php hinzugefügt

---
+++ modules/addons/privacy_policy/lang/german.php
@@ -0,0 +1,44 @@
+<?php
+## v1.5
+$_ADDONLANG['menu']                        = "Menu";
+$_ADDONLANG['privacypolicy']               = "Privacy Policy";
+$_ADDONLANG['addnew']                      = "Add New";
+$_ADDONLANG['savechanges']                 = "Save Changes";
+$_ADDONLANG['multilingualtranslations']    = "Multi-Lingual Translations";
+$_ADDONLANG['sortorder']                   = "Sort Order";
+$_ADDONLANG['sortorderinfo']               = "Enter a number here to override the default sorting";
+$_ADDONLANG['hidden']                      = "Hidden";
+$_ADDONLANG['title']                       = "Title";
+$_ADDONLANG['success']                     = "Changes Saved Successfully!";
+$_ADDONLANG['changessavedsuccessfully']    = "The changes you made have been saved successfully";
+$_ADDONLANG['deletethis']                  = "Are You Sure You Want To Delete This Privacy.";
+$_ADDONLANG['yes']                         = "Yes";
+$_ADDONLANG['no']                          = "No";
+$_ADDONLANG['contents']                    = "Contents";
+$_ADDONLANG['edit']                        = "Edit";
+$_ADDONLANG['menu']                        = "Menu";
+$_ADDONLANG['delete']                      = "Delete";
+$_ADDONLANG['norecordsfound']              = "No Records Found";
+$_ADDONLANG['version']                     = "Version";
+$_ADDONLANG['modulesupport']               = "Module Support";
+$_ADDONLANG['licensekey']                  = "License Key";
+$_ADDONLANG['licenseinformation']          = "License Information";
+$_ADDONLANG['registeredto']                = "Registered To";
+$_ADDONLANG['licensetype']                 = "License Type";
+$_ADDONLANG['validdomain']                 = "Valid Domain";
+$_ADDONLANG['validip']                     = "Valid IP";
+$_ADDONLANG['created']                     = "Created";
+$_ADDONLANG['licensereissued']             = "If you are planning to move your Addons Module to a new location then you will need to get your license reissued.";
+$_ADDONLANG['licenseerror']                = "Sorry but you don't have the right to use this addon model.<br />You can purchase a new license from us just follow the link below.";
+$_ADDONLANG['licenseexpired']              = "Sorry but your license is Expired you can renew the license from us just follow the link below.";
+$_ADDONLANG['licensesuspended']            = "Sorry but your license is Suspended you can contact us just follow the link below.";
+$_ADDONLANG['licenseinvalid']              = "Sorry but your license is Invalid you can contact us just follow the link below.";
+$_ADDONLANG['licensereenter']              = "Or re-enter your license key just";
+$_ADDONLANG['licenseclickhere']            = "Click Here";
+
+## Client Area Language
+
+## v1.5
+$_LANG['pprivacypolicy']                    = "Privacy Policy";
+
+?>
